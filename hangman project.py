a7e7b9fecad1810721ab60ea3f53a8a9a6bc7ed5
hillalee <146629145+hillalee@users.noreply.github.com>
2023-10-01 12:43:24 +0300

    Add files via upload

---
+++ hangman project.py
@@ -0,0 +1,77 @@
+# MAIN PROJECT
+def main():
+    old_letters_guessed = []
+    num_of_tries = 1
+    HANGMAN_PHOTOS = {1: '''
+    x-------x
+    ''',
+                      2: '''
+         x-------x
+    |
+    |
+    |
+    |
+    |
+''', 3: '''    x-------x
+    |       |
+    |       0
+    |
+    |
+    |''', 4: '''
+        x-------x
+    |       |
+    |       0
+    |       |
+    |
+    |''', 5: '''
+        x-------x
+    |       |
+    |       0
+    |      /|\
+    |
+    |''', 6: '''
+       x-------x
+    |       |
+    |       0
+    |      /|\
+    |      /
+    |
+''', 7: '''
+     x-------x
+    |       |
+    |       0
+    |      /|\
+    |      / \
+    |'''}
+
+    from functions import (opening, choose_word, check_win, show_hidden_word, check_valid_input, try_update_letter_guessed)
+    print(opening)
+    # ask user for file path and an index
+    file_path = input("insert file path: ")
+    index = int(input("Insert an index: "))
+    secret_word = choose_word(file_path, index)  # chooses a word
+
+    print(HANGMAN_PHOTOS[num_of_tries])
+
+    # work
+    while num_of_tries < 6 and (check_win(secret_word, old_letters_guessed) == False):
+        show_hidden_word(secret_word, old_letters_guessed)
+        letter_guessed = input("Insert a letter: ")
+        if check_valid_input(letter_guessed, old_letters_guessed):
+            old_letters_guessed = try_update_letter_guessed(letter_guessed, old_letters_guessed)
+            if letter_guessed not in secret_word:
+                num_of_tries += 1
+                print("):")
+                print(HANGMAN_PHOTOS[num_of_tries])
+        else:
+            print("X")
+
+    if check_win(secret_word, old_letters_guessed):
+        print("WIN")
+
+    if num_of_tries == 6:
+        print("LOSE")
+
+
+# vacation.txt
+main()
